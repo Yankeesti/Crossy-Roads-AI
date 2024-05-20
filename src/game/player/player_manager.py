2b2bd47f526a9b25735d5647b89637fcd9fc2c93
Tim Heinsberg <tim.heinsberg@smail.th-koeln.de>
2024-05-20 17:22:37 +0200

    PlayerManager now inherits from pygame.sprite.Group

--- src/game/player/player_manager.py
+++ src/game/player/player_manager.py
@@ -3,17 +3,20 @@ from typing import TYPE_CHECKING
 import pygame
 
 from game.player.player import Player
+
 if TYPE_CHECKING:
     import game.player.controller as controller
     from ..map.road_sections.road_section_base import RoadSectionBase
 
-class PlayerManager:
 
-    def __init__(self,controllers: list[controller.Controller],first_section : RoadSectionBase) -> None:
-        self.players : pygame.sprite.Group = pygame.sprite.Group()
+class PlayerManager(pygame.sprite.Group):
+
+    def __init__(
+        self, controllers: list[controller.Controller], first_section: RoadSectionBase
+    ) -> None:
+        super().__init__()
         for controller in controllers:
-            self.players.add(Player(first_section,controller,self))
-    
+            self.add(Player(first_section, controller, self))
+
     def update(self) -> None:
-        for player in self.players:
-            player.update()
+        self.update()
