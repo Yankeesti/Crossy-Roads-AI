ef0832c40032eec6284086bb919acf17883e8669
Tim Heinsberg <tim.heinsberg@smail.th-koeln.de>
2024-05-28 00:37:53 +0200

    improved fitness function

--- src/neat_training/neural_network_controller.py
+++ src/neat_training/neural_network_controller.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import neat
 import neat.genome
 import game
@@ -5,11 +6,18 @@ import itertools
 
 
 class NeuralNetworkController(game.player.controller.Controller):
-    def __init__(self, genome:neat.genome.DefaultGenome, config):
+    def __init__(self, genome: neat.genome.DefaultGenome, config):
         self.genome = genome
         self.config = config
         self.net = neat.nn.FeedForwardNetwork.create(genome, config)
         self.fitnesses = []
+        self.moves: Dict[game.player_action.PlayerAction, int] = {
+            game.player_action.PlayerAction.STAY: 0,
+            game.player_action.PlayerAction.UP: 0,
+            game.player_action.PlayerAction.DOWN: 0,
+            game.player_action.PlayerAction.LEFT: 0,
+            game.player_action.PlayerAction.RIGHT: 0,
+        }
 
     def get_action(self, inputs):
         flat_list = list(
@@ -23,13 +31,46 @@ class NeuralNetworkController(game.player.controller.Controller):
             )
         )
         output = self.net.activate(tuple(flat_list))
-        return game.player_action.PlayerAction(output.index(max(output)))
+        move = game.player_action.PlayerAction(output.index(max(output)))
+        self.moves[move] += 1
+        return move
 
     def set_fitness(self, fitness):
         self.fitnesses.append(fitness)
 
     def calc_fitness(self):
-        self.genome.fitness = sum(self.fitnesses) / len(self.fitnesses)
+        fitness_unweighted = sum(self.fitnesses) / len(self.fitnesses)
+        # calc prozentual moves
+        total_moves = sum(self.moves.values())
+        percentage_moves = {
+            action: count / total_moves * 100 for action, count in self.moves.items()
+        }
+        if (
+            percentage_moves[game.player_action.PlayerAction.STAY] > 25
+            and percentage_moves[game.player_action.PlayerAction.STAY] < 55
+        ):
+            self.genome.fitness = fitness_unweighted * 0.2
+        if (
+            percentage_moves[game.player_action.PlayerAction.UP] > 25
+            and percentage_moves[game.player_action.PlayerAction.UP] < 55
+        ):
+            self.genome.fitness += fitness_unweighted * 0.2
+        if (
+            percentage_moves[game.player_action.PlayerAction.DOWN] > 0.1
+            and percentage_moves[game.player_action.PlayerAction.DOWN] < 2
+        ):
+            self.genome.fitness += fitness_unweighted * 0.2
+        if (
+            percentage_moves[game.player_action.PlayerAction.LEFT] > 3
+            and percentage_moves[game.player_action.PlayerAction.LEFT] < 14
+        ):
+            self.genome.fitness += fitness_unweighted * 0.2
+        if (
+            percentage_moves[game.player_action.PlayerAction.RIGHT] > 3
+            and percentage_moves[game.player_action.PlayerAction.RIGHT] < 14
+        ):
+            self.genome.fitness += fitness_unweighted * 0.2
+        self.genome.fitness += fitness_unweighted
 
     def get_max_score(self):
         max_value = max(self.fitnesses)
@@ -43,7 +84,9 @@ class NeuralNetworkController(game.player.controller.Controller):
         max_value = max(self.fitnesses)
         out_put: str = ""
         for i in range(max_value, -1, -1):
-            game_indexes = [game_index for game_index, x in enumerate(self.fitnesses) if x == i]
+            game_indexes = [
+                game_index for game_index, x in enumerate(self.fitnesses) if x == i
+            ]
             if len(game_indexes) > 0:
                 out_put += f"{i} in Game(s) {[game_index for game_index, x in enumerate(self.fitnesses) if x == i]}, "
         return out_put
